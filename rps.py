70f27c20ed8d1d3d71491316a73408c948d3bd7f
JohnJCodes <johncj63113@gmail.com>
2025-01-07 17:01:55 -0600

    Remade it

--- rps.py
+++ rps.py
@@ -1,52 +1,41 @@
 from random import randint
-import os
 
-MOVES = ["rock", "paper", "scissors"]
+rules = ["Rock", "Paper", "Scissors"]
 
-GAME_OPTIONS = """
-====================
-0: Exit
-1: Play
-====================
-"""
-
-MOVE_OPTIONS = """
-====================
-Rock
-Paper
-Scissors
-====================
-"""
-
-def game():
-    print("Choose:", MOVE_OPTIONS)
-
-    user_input = input("-> ").lower()
-
-    if not user_input in MOVES:
-        print("Invalid Move")
-        return
-    
-    plr_move = MOVES.index(user_input)
-    bot_move = randint(0, 2)
-
-    results = (3 + plr_move - bot_move) % 3
-    # We do subtraction to find the difference between plr_move and bot_move. (We add 3 to make sure we don't get a negative number.)
-
-    print(f"Bot Picked: {MOVES[bot_move].capitalize()}")
-    print("You Tied" if results == 0 else "You Won" if results == 1 else "You Lost")
+def start_round():
+    u1 = None
+    while not u1 in rules:
+        u1 = str(input("Rock, Paper, or Scissors? : ")).capitalize()
+    u1 = rules.index(u1)
+    bots_move = randint(0,2)
+    print(f"Bot Picked: {rules[bots_move]}")
+    return (u1 - bots_move) % 3
 
 while True:
-    game_mode = None
-    while not game_mode:
-        try:
-            print(GAME_OPTIONS)
-            user_int = int(input("-> "))
-            game_mode = user_int
-        except ValueError:
-            print("ValueError: Invalid Input Value") # If the player's input isn't a integer (int()) the player will get a "Invalid Input Value" error
+    points = 0
+    print("Rock Paper Scissors Best of 3")
+    for i in range(0, 3):
+        winner_of_round = start_round()
+        if winner_of_round == 0:
+            print("Tied Game")
+        elif winner_of_round == 1:
+            points += 1
+            print("You Won")
+        else:
+            print("You Lost")
 
-    if game_mode == 0:
-        exit()
-    elif game_mode == 1:
-        game()
+        print(f"{points}:{}")
+        
+        if (i == 1 or i == 2) and points == 2:
+            print("Congrats! You Win")
+            break
+        elif i == 2 and points < 2:
+            print("You Lose :(")
+            break
+
+    u_continue = None
+    while not u_continue in ["Y", "N"]:
+        u_continue = str(input("Continue? [Y, N]")).capitalize()
+    if not u_continue == "Y":
+        break
+print("Bye, Thanks for Playing!")
